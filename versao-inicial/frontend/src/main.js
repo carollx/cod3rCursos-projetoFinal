7e57a1d1c5c3a9c561c6bcd6961d5c2884fde8aa
unknown <carola198@gmail.com>
2021-08-02 14:09:59 -0300

    Componente do menu do Usuário

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -1,8 +1,19 @@
 import 'font-awesome/css/font-awesome.css'
+// import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import Vue from 'vue'
 
 import App from './App'
+
+import './config/bootstrap'
 import store from './config/store'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCaretDown, faUserCog } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faCaretDown, faUserCog)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
